79eb66bcc7b98263a759c306e26cc78a57d5ad2b
archae0pteryx <github@pocketcereal.com>
2026-01-23 18:46:47 -0800

    fix: use public eyepop-cli-preview repo for downloads

--- Formula/eyepop.rb
+++ Formula/eyepop.rb
@@ -6,11 +6,11 @@ class Eyepop < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/eyepop-ai/eyepop-cli/releases/download/v0.0.1/eyepop-v0.0.1-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/eyepop-ai/eyepop-cli-preview/releases/download/v0.0.1/eyepop-v0.0.1-aarch64-apple-darwin.tar.gz"
       sha256 "d4fc3502c6492f88d426d3840ee3cdafc6df50a3750e1a1ab3764192ed0c79c2"
     end
     on_intel do
-      url "https://github.com/eyepop-ai/eyepop-cli/releases/download/v0.0.1/eyepop-v0.0.1-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/eyepop-ai/eyepop-cli-preview/releases/download/v0.0.1/eyepop-v0.0.1-x86_64-apple-darwin.tar.gz"
       sha256 "e71ab3b45a87082a86ab1cbf082beef94504908c54e9ad0bb339869bad8c0c2e"
     end
   end
